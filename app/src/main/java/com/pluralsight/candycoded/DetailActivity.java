4e106370f3054dd6d887da97eddd7cc50a3ad31a
Hatim abuirtaymah <abourtemah@gmail.com>
2021-08-11 20:23:26 +0200

    update Share a Candy with an Intent

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -78,6 +78,9 @@ public class DetailActivity extends AppCompatActivity {
     // TODO - Task 4 - Share the Current Candy with an Intent
     // ***
     private void createShareIntent(){
-        startActivity(new Intent(Intent.ACTION_SEND));
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
+        startActivity(shareIntent);
     }
 }
